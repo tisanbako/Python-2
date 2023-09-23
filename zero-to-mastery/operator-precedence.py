c6ecf719b28d2fb6347e5bcef646c98982aa25da
tisanbako <bakotisan@gmail.com>
2023-09-23 17:33:08 -0400

    updating operator file

--- zero-to-mastery/operator-precedence.py
+++ zero-to-mastery/operator-precedence.py
@@ -8,3 +8,7 @@ print(20 * (3-1))
 #2. ** raise to the power gets processed second
 #3. * / multiply and division next
 #4. + - plus and minus comes last
+
+print(bin(5)) #to get the binary number (0 & 1) of 5. 
+#note, output starts with 0b meaning binary but you can google the binary of 5
+print(int("101", 2)) #this is to change binary to int. 2 stands for binary language (base 2) humans use base 10
