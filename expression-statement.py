fb0d9513fe87957d85fcd4517e1c72f94919b1a2
tisanbako <bakotisan@gmail.com>
2023-09-23 19:10:15 -0400

    adding expression and statement file

--- expression-statement.py
+++ expression-statement.py
@@ -1,3 +1,4 @@
 #Expression is a piece of code that produces value eg
 iq = 90
-user_score = iq/5 # here 1q/5 is an expression ad it produces avalue of 
+user_score = iq//5 # here 1q/5 is an expression ad it produces avalue of 
+print(user_score)
