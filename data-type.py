5b6795c8f684e991bcecccdb57c741d00c865e01
tisanbako <bakotisan@gmail.com>
2023-09-23 19:06:37 -0400

    adding expression and statement files

--- data-type.py
+++
@@ -1,6 +0,0 @@
-item_quantity = "10"
-
-print (type(item_quantity))
-
-new_data_type = int(item_quantity)
-print (type(new_data_type))
--- zero-to-mastery/variable.py
+++ zero-to-mastery/variable.py
@@ -1,3 +1,11 @@
 iq = 90
+print(iq)
 user_age = iq/4
+print(user_age)
 a = user_age 
+print(a)
+a,b,c = 1,2,3
+print(a)
+print(b)
+print(c)
+print(a, b)
