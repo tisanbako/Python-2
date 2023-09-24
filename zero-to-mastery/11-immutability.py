c9f740947e933c5f084d555f611573ff7ac69c93
tisanbako <bakotisan@gmail.com>
2023-09-24 17:47:18 -0400

    creating immutable file

---
+++ zero-to-mastery/11-immutability.py
@@ -0,0 +1,12 @@
+name = "jeremiah"
+
+#we can change the value of name to be something else eg
+name = "bako"
+
+print (name)
+
+#we changed the value of name from Jeremiah to bako
+#but we cannot change the characters in the spelling of Jeremia eg
+# name = "jeremiah"
+# name[0] = "g" #trying to change fomr jeremiah to geremiah
+#print(name) it will give erroe as tring objects not suppor item assignmemt
