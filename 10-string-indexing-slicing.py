2d20f885d470b000b309b4f1b7623550523363b7
tisanbako <bakotisan@gmail.com>
2023-09-24 17:20:15 -0400

    updating 10-string-indexing-slicing.py

--- 10-string-indexing-slicing.py
+++ 10-string-indexing-slicing.py
@@ -4,16 +4,18 @@ print(selfish[0]) #m
 print(selfish[1]) #2
 print(selfish[2]) # space
 
+
+#SLICING
 property = "abcdefghi"
            #123456789
 #[start:stop at:stepover]
 print(property[2:9:1])   #cdefghi
 print(property[0:9:1])   #abcdefghi  
-print(property[::1]) #abcdefghi
-print(property[1:9:1]) #bcdefghi
-print(property[1:]) #bcdefghi
-print(property[:3])    #  abc    
-print(property[1:8:2]) #bdfh
-print(property[-1]) #1 printing on reverse mode
-print(property[::-1]) #ihgfedcba
-print(property[::-2]) #igeca
+# print(property[::1]) #abcdefghi
+# print(property[1:9:1]) #bcdefghi
+# print(property[1:]) #bcdefghi
+# print(property[:3])    #  abc    
+# print(property[1:8:2]) #bdfh
+# print(property[-1]) #1 printing on reverse mode
+# print(property[::-1]) #ihgfedcba
+# print(property[::-2]) #igeca
