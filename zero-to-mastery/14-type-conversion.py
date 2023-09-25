e31dd50122beeda02cd2bcef8e59f47c56f694e3
tisanbako <bakotisan@gmail.com>
2023-09-24 22:51:49 -0400

    adding type c0nversion file

---
+++ zero-to-mastery/14-type-conversion.py
@@ -0,0 +1,16 @@
+#Lets say we have a facebook page with the info
+name = "Jeremiah Bako"
+age = 19
+relationship_status = "single"
+
+#Lets say my relationship status changes
+relationship_status = "it's single"
+print(relationship_status)
+
+#ex2 
+
+birth_year = input("what year were you born? ") #your birthyear is input as a str
+
+age = 2023 - int(birth_year) #converting birth yeat to int
+
+print(f"your age is: {age}")
