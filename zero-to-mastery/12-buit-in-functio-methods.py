6f5adb57e1062bf8641c63912ccbf3f140c5f5de
tisanbako <bakotisan@gmail.com>
2023-09-24 21:52:21 -0400

    adding bool file

--- zero-to-mastery/12-buit-in-functio-methods.py
+++ zero-to-mastery/12-buit-in-functio-methods.py
@@ -2,12 +2,43 @@
 #built in methods are owned by something
 #methods starts with a dot eg .append() .capitalize() .slit() 
 #while gunctions can be len() round() range() type() print() input() int() str()
+#function comes before the variable and methods come after the variable
+
+name = "benjamin"
+print(len(name)) #function
+print(name.capitalize()) #method
 
 #FUNCTION
-name = "benjamin"
-print(len(name))
-print(name.capitalize())
-#print(range(name))
 
-numbers = 2394
-print(range(numbers))
+print(len("hellllloooo"))
+
+greet = "hellloooo"
+print(greet[:]) #hellloooo
+print(greet[0:len(greet)]) #hellloooo
+print(greet[0:9]) #slicing
+
+#METHODS
+quote = "to god be the glory"
+print(quote.upper()) #print everything in uppercase (TO GOD BE THE GLORY)
+print(quote.capitalize()) #to capitalize the first word (To god be the glory)
+print(quote.lower())
+print(quote.find("be")) #check if there's be and the index
+print(quote.replace("be", "i give all")) #replace be with i give all
+
+
+#All these 5 lines above are not variables, they arejust creating a new stings but we are not assigning it to a variable
+#that way, we still have the value of quote to be (to god be the glory)
+
+print(quote) #(to god be the glory)despite all the function or method the value of
+#quote didnt change because strings are immutable we can't change. we can either them
+# if we want to change the value of quote, we have to overite with a new value for quote eg
+#quote = "my god is good"  
+
+#we can change the value of quote or assign a new value we can
+# do it in many ways eg
+
+quote = "my new quote"
+print(quote)
+
+quote = quote.upper()
+print(quote)
---
+++ zero-to-mastery/13-boolean.py
@@ -0,0 +1,9 @@
+#booleans
+#booleans uses True or False. bool is the conerstone computer scienece.
+#we tell our program to do something if something is True or False
+#we use boolean to denote logic
+
+#conver 1 and 0 to boolean 
+
+print(bool(1))
+print(bool(0))
