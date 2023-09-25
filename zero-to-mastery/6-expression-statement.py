d63876812c87b35aeb8000b8df9e1a7f5d114b1d
tisanbako <bakotisan@gmail.com>
2023-09-24 23:05:09 -0400

    updating files

--- zero-to-mastery/6-expression-statement.py
+++ zero-to-mastery/6-expression-statement.py
@@ -3,4 +3,4 @@ iq = 90
 user_score = iq//5 # here 1q/5 is an expression ad it produces avalue of  18
 print(user_score)
 
-#statement is a whole like of code eg iq = 90 or user_score = iq//5 
+#statement is a whole line of code eg iq = 90 or user_score = iq//5 
