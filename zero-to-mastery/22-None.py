372e7416d78835710a91bcdeb7c18636baba2786
tisanbako <bakotisan@gmail.com>
2023-09-26 23:12:53 -0400

    updating none.py file

--- zero-to-mastery/22-None.py
+++ zero-to-mastery/22-None.py
@@ -1,1 +1,6 @@
-#None represents the absense
+#None represents the absense of value 
+#example is when you're creating a video game and the user starts with no 
+#weapon, we can code it by saying
+
+weapon = None
+print(weapon)    #None 
