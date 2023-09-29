be0b320fdfd3de0b647bb43a5d45921d52dd7f51
tisanbako <bakotisan@gmail.com>
2023-09-29 14:41:43 -0400

    updating matrix file

--- zero-to-mastery/17-matrix.py
+++ zero-to-mastery/17-matrix.py
@@ -8,30 +8,30 @@ d3 = [[1,2,3], [[0,4,7],4,5,6], [7,[4,5,4],8,6]]
 print(d3[1][0])  #my experiment prints [0, 4, 7] google how to access an array in an array
 
 
-# #accessing a multidimentional list eg
-# mt3 = [[1,2,3], [4,5,6], [7,8,9]]  
-# print(mt3[0][1])   #2  access the first time [1,2,3] which is the first array and 1 index in the first array
-# print(mt3[2][2])              #9
-# print(mt3[0][0])              #1
-# print(mt3[1][2])               #6
+#accessing a multidimentional list eg
+mt3 = [[1,2,3], [4,5,6], [7,8,9]]  
+print(mt3[0][1])   #2  access the first time [1,2,3] which is the first array and 1 index in the first array
+print(mt3[2][2])              #9
+print(mt3[0][0])              #1
+print(mt3[1][2])               #6
 
 
 
 
-# #EXERCISE
+#EXERCISE
 
-# A = [[1, 4, 5, 12], 
-#     [-5, 8, 9, 0],
-#     [-6, 7, 11, 19]]
+A = [[1, 4, 5, 12], 
+    [-5, 8, 9, 0],
+    [-6, 7, 11, 19]]
 
-# print("A =", A) 
-# print("A[1] =", A[1])      # 2nd row
-# print("A[1][2] =", A[1][2])   # 3rd element of 2nd row
-# print("A[0][-1] =", A[0][-1])   # Last element of 1st Row
+print("A =", A) 
+print("A[1] =", A[1])      # 2nd row
+print("A[1][2] =", A[1][2])   # 3rd element of 2nd row 9
+print("A[0][-1] =", A[0][-1])   # Last element of 1st Row 12
 
-# column = [];        # empty list
-# for row in A:
-#   column.append(row[2])
+column = [];        # empty list
+for row in A:
+  column.append(row[2])
   
-# print("3rd column =", column)
+print("3rd column =", column)
 
