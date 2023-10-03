e060243674c56964e8782f044be89ede8fc0d829
tisanbako <bakotisan@gmail.com>
2023-10-03 14:12:48 -0400

    adding loops file

--- 29-loops-if-else.py
+++ 29-loops-if-else.py
@@ -1,13 +1,13 @@
-# is_old_enough = False
-# is_licence = True 
+is_old_enough = False
+is_licence = True 
 
-# if is_old_enough: # = True
-#     print("you are old enough to drive")
-# elif is_licence:
-#     print("you can drive now")
+if is_old_enough: # = True
+    print("you are old enough to drive")
+elif is_licence:
+    print("you can drive now")
 
-# else:
-#     print("you're too young to drive")    
+else:
+    print("you're too young to drive")    
 
 #What happens if someone has alicence and is not old enough, ot old enough but dont have a 
 # license? so what we have about is not ideal we need a both licence and age to be True at same time
@@ -22,4 +22,11 @@ if old_enough and has_licence: #: means true
 else:
     print("you are not of age")    
 
-#Another way to do the if else logic is using Ternary Operator   
+#Another way to do the if else logic is using Ternary Operator (Conditional expression)  
+#This a shortcut and can be use for some if else statement..
+
+#condition_if_true if condition else condition_if_else
+#eg
+is_friend = True
+can_message = "message allowed" if is_friend else "not allowed to message"
+print(can_message)     #change -s_friend to False and see
